9f28ecb7b1dc582cee7cb5aa44607d881780a61c
慕枫 <javakang@qq.com>
2020-10-28 17:31:28 +0800

    优化下

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:typed_data';
 
-import 'package:dart_json_mapper/dart_json_mapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_flutter_systray/go_flutter_systray.dart';
